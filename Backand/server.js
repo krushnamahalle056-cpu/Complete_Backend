c94e24b0ec7b790b7b7f2fb4be50f129267cf332
Krushna Mahalle <krushnamahalle056@gmail.com>
2026-09-18 18:00:37 +0530

    rename the index.js to server.js

--- Backand/server.js
+++ Backand/server.js
@@ -4,16 +4,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('HI Krushna Mahalle');
+  res.send('server created');
 });
 
-app.get('/netflix',(req, res) =>{
-    res.send("<h2>Welcome to Netflix</h2>");
-})
 
-app.get('/login',(req, res) =>{
-    res.send(`<h1>Welcome to Login Page</h1>`);
-})
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT || 4000}`);
